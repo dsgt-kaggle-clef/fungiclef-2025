22befb4d9bbf4ab772881eaf57be5fa238333ae3
Jason Kahei Tam <jasonkhtam77@gmail.com>
2025-02-24 20:10:54 -0500

    refactored notebook to reference scratch

--- user/tamncheese/generate_trial_stratified_trial.py
+++ user/tamncheese/generate_trial_stratified_trial.py
@@ -5,22 +5,30 @@ from sklearn.model_selection import train_test_split
 # generating a smaller subset for
 seed = 2025
 
-#stratified training set 10%
-df = pd.read_csv('FungiCLEF2023_train_metadata_PRODUCTION.csv')
+# stratified training set 10%
+df = pd.read_csv(
+    "/storage/scratch1/5/jtam30/FungiCLEF2023_train_metadata_PRODUCTION.csv"
+)
 label = "species"
+print("hi")
 print(f"Dataset shape: {df.shape}")
 df = df.dropna(subset=[label]).reset_index(drop=True)
 print(f"Dataset shape: {df.shape}")
 print(f"Target column shape: {df['species'].shape}")  # Ensure both match
 
 #
-trial_df, temp_df = train_test_split(df, test_size = 0.9, random_state = seed, stratify = df[label])
+trial_df, temp_df = train_test_split(
+    df, test_size=0.99, random_state=seed, stratify=df[label]
+)
 
 print(f"Train set size: {len(trial_df)}")
 
-print("Class distribution in training_laptop set:")
+print("Class distribution in training set:")
 print(trial_df[label].value_counts(normalize=True))
 print("Class distribution in full set:")
 print(df[label].value_counts(normalize=True))
 
-trial_df.to_csv('_dataset_trial/metadata_trial.csv', index=False)
+trial_df.to_csv(
+    "/storage/home/hcoda1/5/jtam30/clef/fungiclef-2025/user/tamncheese/_dataset_trial/metadata_trial.csv",
+    index=False,
+)
